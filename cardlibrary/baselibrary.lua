886b97a4350013ff3a84786c818f6b626d2e9287
Llendlar <lturk32765@gmail.com>
2018-02-14 14:41:11 -0500

    Update baselibrary.lua

--- cardlibrary/baselibrary.lua
+++ cardlibrary/baselibrary.lua
@@ -5429,7 +5429,7 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	},
 	
 	["Disaster Survival"] = {
-		["Id"] = 1401986621,
+		["Id"] = 1421811561,
 		["Name"] = "Disaster Survival",
 		["Health"] = 0,
 		["Power"] = 0,
@@ -5438,7 +5438,7 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 		["Field"] = 293402597,
 		["Archetype"] = "Terrain",
 		["Color"] = "Neutral",
-		["Cost"] = {["Red"] = 1},
+		["Cost"] = {["Red"] = 2},
 		["Effect"] = {
 			Name = "Stud",
 			Description = "While this card is in play. Deal 50 damage to all fighters at the start of each turn.",
